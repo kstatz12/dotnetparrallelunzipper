736d4258edfd2000680ddb4bc3186ecd6b9ce0b6
kstatz <kstatz@core12.com>
2014-03-08 11:05:16 -0600

    cleaned up unused method, added app config

--- DotNetUnZipper/DotNetUnZipper/Program.cs
+++ DotNetUnZipper/DotNetUnZipper/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,7 @@ namespace DotNetUnZipper
     {
         static void Main(string[] args)
         {
-            var dirPath = @"c:\test\testZips";
+            var dirPath = ConfigurationSettings.AppSettings["SourceDirectory"];
             var di = new DirectoryInfo(dirPath);
             var files = di.GetFiles("*.zip");
             Parallel.ForEach(files, currentfile =>
@@ -24,52 +25,31 @@ namespace DotNetUnZipper
                 Console.WriteLine("Decompressing {0} on thread {1}", currentfile, Thread.CurrentThread.ManagedThreadId);
             });
         }
-
-        public static void Decompress(FileInfo file)
+        private static void Extract(FileInfo file)
         {
-            using (var inFile = file.OpenRead())
-            {
-                var curFile = file.FullName;
-                var origName = curFile.Remove(curFile.Length - file.Extension.Length);
-
-                using (var outfile = File.Create(origName))
-                {
-
-                    using (var decompress = new GZipStream(inFile, CompressionMode.Decompress))
-                    {
-                        decompress.CopyTo(outfile);
-                    }
-                }
-            }
-        }
-
-        public static void Extract(FileInfo file)
-        {
-            string outfile = @"c:\test\unzipped";
-            ZipFile zf = null;
+            var outfile = ConfigurationSettings.AppSettings["DestinationDirectory"];
+            ZipFile zipFile = null;
             try
             {
                 using (var fs = file.OpenRead())
                 {
-                    zf = new ZipFile(fs);
-                    foreach (ZipEntry entry in zf)
+                    zipFile = new ZipFile(fs);
+                    foreach (ZipEntry zipEntry in zipFile)
                     {
-                        if (!entry.IsFile)
+                        if (!zipEntry.IsFile)
                         {
                             continue;
                         }
-                        var entryFileName = entry.Name;
+                        var entryFileName = zipEntry.Name;
                         var buffer = new byte[4096];
-                        var zipStream = zf.GetInputStream(entry);
+                        var zipStream = zipFile.GetInputStream(zipEntry);
                         var fullZipToPath = Path.Combine(outfile, entryFileName);
                         var directoryName = Path.GetDirectoryName(fullZipToPath);
-
-                        if (directoryName.Length > 0)
+                        if (!string.IsNullOrEmpty(directoryName))
                         {
                             Directory.CreateDirectory(directoryName);
                         }
-
-                        using (FileStream streamWriter = File.Create(fullZipToPath))
+                        using (var streamWriter = File.Create(fullZipToPath))
                         {
                             StreamUtils.Copy(zipStream, streamWriter, buffer);
                         }
@@ -78,14 +58,12 @@ namespace DotNetUnZipper
             }
             finally
             {
-                if (zf != null)
+                if (zipFile != null)
                 {
-                    zf.IsStreamOwner = true;
+                    zipFile.IsStreamOwner = true;
+                    zipFile.Close();
                 }
-                zf.Close();
             }
         }
-
-        
     }
 }
